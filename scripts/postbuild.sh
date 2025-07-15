9bb6fdd93469f98028f1655d25f82af6d7550c5d
Lucas Yang <yangchenshin77@gmail.com>
2025-07-16 04:17:58 +0800

    Transform mkdist to tsdown

--- scripts/postbuild.sh
+++
@@ -1,4 +0,0 @@
-rm \
-  dist/pagination-astro/stub \
-  dist/pagination-vue/stub \
-  -rf
---
+++ tsdown.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: [
+    './src/api/index.ts',
+    './src/company-setting/index.ts',
+    './src/form-validator/index.ts',
+    './src/image/index.ts',
+    './src/page/index.ts',
+    './src/pagination-astro/index.ts',
+    './src/pagination-vue/index.ts',
+    './src/product-variant/index.ts',
+    './src/query-params/index.ts',
+  ],
+  copy: [
+    { from: 'src/image/components', to: 'dist/image/components' },
+    { from: 'src/page/components', to: 'dist/page/components' },
+    { from: 'src/query-params/components', to: 'dist/query-params/components' },
+  ],
+  dts: true,
+})
